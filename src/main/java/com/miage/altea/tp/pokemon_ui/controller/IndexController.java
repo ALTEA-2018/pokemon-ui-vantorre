4bc6ef554e50fbebd11607406a193406e6cc04e1
fabien vantorre <fabien.vantorre@hotmail.fr>
2019-02-11 09:40:49 +0100

    header

--- src/main/java/com/miage/altea/tp/pokemon_ui/controller/IndexController.java
+++ src/main/java/com/miage/altea/tp/pokemon_ui/controller/IndexController.java
@@ -4,7 +4,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.View;
 
 @Controller
 public class IndexController {
@@ -15,7 +14,7 @@ public class IndexController {
     }
 
     @PostMapping("/registerTrainer")
-    ModelAndView registerNewTrainer(String trainerName){
+    ModelAndView registerNewTrainer(String trainerName) {
         ModelAndView res = new ModelAndView();
         res.addObject("name", trainerName);
         res.setViewName("register");
