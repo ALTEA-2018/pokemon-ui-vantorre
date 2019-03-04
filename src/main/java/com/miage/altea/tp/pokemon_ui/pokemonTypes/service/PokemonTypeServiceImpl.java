ad1b71df725c2af10548fb340190e5613469bfbf
fabien vantorre <fabien.vantorre@hotmail.fr>
2019-03-05 00:03:28 +0100

    passage locale en pathvariable

--- src/main/java/com/miage/altea/tp/pokemon_ui/pokemonTypes/service/PokemonTypeServiceImpl.java
+++ src/main/java/com/miage/altea/tp/pokemon_ui/pokemonTypes/service/PokemonTypeServiceImpl.java
@@ -3,11 +3,13 @@ package com.miage.altea.tp.pokemon_ui.pokemonTypes.service;
 import com.miage.altea.tp.pokemon_ui.pokemonTypes.bo.PokemonType;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Locale;
 
 @Service
 public class PokemonTypeServiceImpl implements PokemonTypeService {
@@ -16,7 +18,8 @@ public class PokemonTypeServiceImpl implements PokemonTypeService {
     private String pokemonServiceUrl;
 
     public List<PokemonType> listPokemonsTypes() {
-        return Arrays.asList(this.restTemplate.getForObject(this.pokemonServiceUrl + "/pokemon-types/", PokemonType[].class));
+        Locale locale = LocaleContextHolder.getLocale();
+        return Arrays.asList(this.restTemplate.getForObject(this.pokemonServiceUrl + "/pokemon-types?locale=" + locale, PokemonType[].class));
     }
 
     @Autowired
