d9f136267a8ddf820b00aba9c9fe307375649103
fabien vantorre <fabien.vantorre@hotmail.fr>
2019-03-11 10:39:49 +0100

    interceptor

--- src/main/java/com/miage/altea/tp/pokemon_ui/config/RestConfiguration.java
+++ src/main/java/com/miage/altea/tp/pokemon_ui/config/RestConfiguration.java
@@ -1,15 +1,32 @@
 package com.miage.altea.tp.pokemon_ui.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.ClientHttpRequestInterceptor;
+import org.springframework.http.client.support.BasicAuthenticationInterceptor;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Configuration
 public class RestConfiguration {
 
+
+    @Value("${trainer.service.username}")
+    private String username;
+
+    @Value("${trainer.service.password}")
+    private String password;
+
     @Bean
     RestTemplate restTemplate(){
-        return new RestTemplate(); 
+        RestTemplate template= new RestTemplate();
+        List<ClientHttpRequestInterceptor> interceptors = new ArrayList<>();
+        interceptors.add(new BasicAuthenticationInterceptor(username, password));
+        template.setInterceptors(interceptors);
+        return template;
     }
 
 }
